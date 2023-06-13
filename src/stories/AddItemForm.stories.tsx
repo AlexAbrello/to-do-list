9c0f4261400700b55727b77419cf909e035d31c5
AlexAbrello <abrello063@gmail.com>
2023-06-13 10:53:47 +0300

    lesson #11

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -2,6 +2,10 @@ import type { Meta, StoryObj } from '@storybook/react';
 import {action} from '@storybook/addon-actions'
 
 import {AddItemForm} from "../AddItemForm";
+import TextField from "@mui/material/TextField/TextField";
+import {IconButton} from "@mui/material";
+import {AddBox} from "@mui/icons-material";
+import React from "react";
 
 const meta: Meta<typeof AddItemForm> = {
   title: 'TODOLISTS/AddItemForm',
@@ -23,3 +27,19 @@ export const AddItemFormStory: Story = {
     addItem: action('Button clicked inside form')
   }
 };
+
+export const AddItemFormErrorStory: Story = {
+  render: (args => <div>
+    <TextField variant="outlined"
+               error={true}
+               value={''}
+               onChange={() => {}}
+               onKeyPress={() => {}}
+               label="Title"
+               helperText={"Title is required"}
+    />
+    <IconButton color="primary" onClick={args.addItem}>
+      <AddBox />
+    </IconButton>
+  </div>)
+}
