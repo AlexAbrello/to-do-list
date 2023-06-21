396c2c16aaaf3c16972697b52211a074705e7cc7
AlexAbrello <abrello063@gmail.com>
2023-06-21 14:43:48 +0300

    lesson #13 DAL refactor(types)

--- src/api/todoist-api.tsx
+++ src/api/todoist-api.tsx
@@ -7,15 +7,28 @@ const instance = axios.create({
 
 export const todolistAPI = {
   getTodolists() {
-    return instance.get(`todo-lists`)
+    return instance.get<TodoListType[]>(`todo-lists`)
   },
   createTodolist(title: string) {
-    return instance.post('todo-lists', {title})
+    return instance.post<ResponseType<{item: TodoListType}>>('todo-lists', {title})
   },
   deleteTodolist(todolistId: string) {
-    return instance.delete(`todo-lists/${todolistId}`)
+    return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
   },
   updateTodolist(todolistId: string, title: string) {
-    return instance.put(`todo-lists/${todolistId}`, {title})
+    return instance.put<ResponseType>(`todo-lists/${todolistId}`, {title})
   },
+}
+
+type TodoListType = {
+  id: string
+  title: string
+  addedDate: Date
+  order: number
+}
+
+type ResponseType<T = {}> = {
+  resultCode: number
+  messages: string[]
+  data: T
 }
