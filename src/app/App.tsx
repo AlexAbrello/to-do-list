685afd3e200f440bb610d0f96caeb9baa8c51a7c
AlexAbrello <abrello063@gmail.com>
2023-07-12 16:08:48 +0300

    lesson#16 Login Routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { TodolistsList } from '../features/TodolistsList/TodolistsList'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
 
 // You can learn about the difference by reading this guide on minimizing bundle size.
 // https://mui.com/guides/minimizing-bundle-size/
@@ -10,41 +10,45 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import { Menu } from '@mui/icons-material';
+import {Menu} from '@mui/icons-material';
 import LinearProgress from "@mui/material/LinearProgress/LinearProgress";
 import {useAppSelector} from "./store";
 import {RequestStatusType} from "./app-reducer";
 import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
 import {Login} from "../features/Login/Login";
+import {Route, Routes} from "react-router-dom";
 
 
 function App() {
 
   const loading = useAppSelector<RequestStatusType>(state => state.app.status)
 
-    return (
-        <div className="App">
-          <ErrorSnackbar />
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-              <div className='linearProgress'>
-                {loading === 'loading' && <LinearProgress color='error'/>}
-              </div>
-            </AppBar>
-            <Container fixed>
-                <TodolistsList/>
-            </Container>
-          <Login />
-        </div>
-    )
+  return (
+      <div className="App">
+        <ErrorSnackbar/>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <Menu/>
+            </IconButton>
+            <Typography variant="h6">
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+          <div className='linearProgress'>
+            {loading === 'loading' && <LinearProgress color='error'/>}
+          </div>
+        </AppBar>
+        <Container fixed>
+          <Routes>
+            <Route path='/' element={<TodolistsList/>}/>
+            <Route path='/login' element={<Login/>}/>
+          </Routes>
+        </Container>
+
+      </div>
+  )
 }
 
 export default App
