408ec41686beb0deb20134233557a6120cafcf06
AlexAbrello <abrello063@gmail.com>
2023-08-01 13:37:32 +0300

    lesson#19 refactor: structure of project

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 import {tasksReducer} from 'features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from 'features/TodolistsList/todolists-reducer';
-import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux'
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
+import {AnyAction, combineReducers} from 'redux'
+import {ThunkDispatch} from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from 'features/Login/auth-reducer'
 import {configureStore} from "@reduxjs/toolkit";
@@ -21,7 +21,7 @@ export const store = configureStore({
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof store.getState>
 
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>
+// export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>
 
 // export type AppDispatch = typeof store.dispatch
 export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
